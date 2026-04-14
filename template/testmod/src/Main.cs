016f4e5ebbac9a7a24aaba1c11b5db9b3c995c44
Nathan Williams <nathanrhyswilliams@hotmail.co.uk>
2026-04-14 02:20:29 +0000

    hopefully 4th times the charm - updated main.cs with harmony

--- template/testmod/src/Main.cs
+++ template/testmod/src/Main.cs
@@ -1,9 +1,13 @@
-namespace testmod
-{
-    public class Main : IModApi
-    {
-        public void InitMod(Mod modInstance)
-        {
+using HarmonyLib;
+
+namespace testmod {
+    public class Main : IModApi {
+        
+        private static Harmony? _harmony;
+
+        public void InitMod(Mod modInstance) {
+            _harmony ??= new Harmony("GluckHouse.testmod");
+            _harmony.PatchAll();
             Log.Out("testmod initialized.");
         }
     }
